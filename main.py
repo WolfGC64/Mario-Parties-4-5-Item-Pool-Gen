a75b7aa1441a050345dd57865ba26988f87ca117
Rainchus <rainmachus@gmail.com>
2023-05-21 20:59:47 -0500

    now generates mp4 us and mp5 pal

--- main.py
+++ main.py
@@ -26,14 +26,14 @@ bctrl
 
 """
 
-gecko_code_header = f"""
+gecko_code_header_mp5_pal = f"""
 C20C9968 00000024
 9421FFF0 7C0802A6
 90010014 93E10000
 48000081 60000000
 """
 
-gecko_code_footer = f"""
+gecko_code_footer_mp5_pal = f"""
 7CE802A6 38E70004
 38600000 38800000
 2C030074 41820014
@@ -53,6 +53,36 @@ gecko_code_footer = f"""
 7C0803A6 38210010
 4E800020 00000000
 """
+
+gecko_code_header_mp4_us = f"""
+C2083878 0000001E
+48000041 60000000
+"""
+
+gecko_code_footer_mp4_us = f"""
+7CE802A6 38600000
+38800000 2C030034
+41820014 7CA71A2E
+7C842A14 38630004
+4BFFFFEC 3CA08005
+60A5FB40 7CA903A6
+7C832378 4E800421
+38800000 38A00000
+2C050034 41820024
+7CC72A2E 7C661850
+2C030000 40A00008
+48000010 38840001
+38A50004 4BFFFFDC
+1C840004 38840002
+7C87222E 3CA0817F
+60A5FFF0 90850000
+2C040008 41800008
+38840001 3CA00007
+60A5006D 7C642A14
+38800000 38A00000
+38A00000 00000000
+"""
+
 #define dictionaries
 button_vars = {}
 entry_boxes = {}
@@ -106,42 +136,56 @@ button_texts_mp4 = {
     "Magic Lamp": "C",          # 0000000C
 }
 
-def clear_selections():
-    for i, var in enumerate(button_vars):
-        entry_boxes[i].delete(0, tk.END)
-        entry_boxes[i].insert(0, '0')
+def clear_selections(tab_name):
+    for i, var in enumerate(button_vars[tab_name]):
+        entry_boxes[tab_name][i].delete(0, tk.END)
+        entry_boxes[tab_name][i].insert(0, '0')
+        check_buttons[tab_name][i].config(fg='grey') # Set the color to a lighter one when not checked
         var.set(0)
 
-def set_button_and_entry(index, weight, checked):
-    button_vars[index].set(checked)
-    entry_boxes[index].delete(0, tk.END)
-    entry_boxes[index].insert(0, str(weight))
+def set_button_and_entry(tab_name, index, weight, checked):
+    button_vars[tab_name][index].set(checked)
+    entry_boxes[tab_name][index].delete(0, tk.END)
+    entry_boxes[tab_name][index].insert(0, str(weight))
     if checked == 0:
-        check_buttons[index].config(fg='grey')
+        check_buttons[tab_name][index].config(fg='grey')
     else:
-        check_buttons[index].config(fg='black')
+        check_buttons[tab_name][index].config(fg='black')
 
-def save_csv():
+def save_csv(tab_parent):
     file_path = filedialog.asksaveasfilename(defaultextension='.csv', filetypes=[('CSV Files', '*.csv')])
-
     if file_path:
-        button_text_keys = list(button_texts_mp5.keys())
+        selected_tab = tab_parent.select()  # Get currently selected tab ID
+        selected_tab_text = tab_parent.tab(selected_tab, "text")  # Get the text of the selected tab
+        if selected_tab_text == "MP4":
+            button_text_keys = list(button_texts_mp4.keys())
+        elif selected_tab_text == "MP5":
+            button_text_keys = list(button_texts_mp5.keys())
+
         with open(file_path, 'w', newline='') as file:
             fieldnames = ['name', 'weight', 'on/off']
             writer = csv.DictWriter(file, fieldnames=fieldnames)
             writer.writeheader()
             for i in range(len(button_text_keys)):
                 name = button_text_keys[i]
-                weight = int(entry_boxes[i].get())
-                checked = button_vars[i].get()
+                weight = int(entry_boxes[selected_tab_text][i].get())
+                checked = button_vars[selected_tab_text][i].get()
 
                 writer.writerow({'name': name, 'weight': weight, 'on/off': checked})
 
-def load_csv():
+
+def load_csv(tab_parent):
     file_path = filedialog.askopenfilename(filetypes=[('CSV Files', '*.csv')])
     if file_path:
-        clear_selections()
-        button_text_keys = list(button_texts_mp5.keys())
+        selected_tab = tab_parent.select()  # Get currently selected tab ID
+        selected_tab_text = tab_parent.tab(selected_tab, "text")  # Get the text of the selected tab
+        if selected_tab_text == "MP4":
+            clear_selections("MP4")
+            button_text_keys = list(button_texts_mp4.keys())
+        elif selected_tab_text == "MP5":
+            clear_selections("MP5")
+            button_text_keys = list(button_texts_mp5.keys())
+
         with open(file_path, 'r') as file:
             csv_reader = csv.DictReader(file)
             for row in csv_reader:
@@ -150,26 +194,43 @@ def load_csv():
                 checked = int(row['on/off'])
                 if name in button_text_keys:
                     i = button_text_keys.index(name)
-                    set_button_and_entry(i, weight, checked)
-                    on_checkbutton_change(i)  # Update checkbox color
+                    set_button_and_entry(selected_tab_text, i, weight, checked)
+                    on_checkbutton_change(i, selected_tab_text)  # Update checkbox color
 
-def generate_gecko_code():
-    selected_tab = event.widget.select()  # Get currently selected tab ID
-    selected_tab_text = event.widget.tab(selected_tab, "text")  # Get the text of the selected tab
+
+def generate_gecko_code(tab_parent):
+    selected_tab = tab_parent.select()  # Get currently selected tab ID
+    selected_tab_text = tab_parent.tab(selected_tab, "text")  # Get the text of the selected tab
     
     # Draw the appropriate grid
     if selected_tab_text == "MP5":
         button_text_keys = list(button_texts_mp5.keys())
+        entry_boxes_dict = entry_boxes["MP5"]
+        button_texts_dict = button_texts_mp5
+        gecko_code_header = gecko_code_header_mp5_pal
+        gecko_code_footer = gecko_code_footer_mp5_pal
     elif selected_tab_text == "MP4":
         button_text_keys = list(button_texts_mp4.keys())
+        entry_boxes_dict = entry_boxes["MP4"]
+        button_texts_dict = button_texts_mp4
+        gecko_code_header = gecko_code_header_mp4_us
+        gecko_code_footer = gecko_code_footer_mp4_us
+    else:
+        return
+
     code_str = ""
 
     for i in range(len(button_text_keys)):
-        current_weight = int(entry_boxes[i].get())
+        if i >= len(entry_boxes_dict):
+            break
+
+        current_weight = int(entry_boxes_dict[i].get())
         # Add weight as a 2-byte hexadecimal
         weight_hex = format(current_weight, '04x')
-        # Add item id
-        item_id = format(int(button_texts_mp5[button_text_keys[i]], 16), '04x')
+        # Get the item name from button_text_keys
+        item_name = button_text_keys[i]
+        # Get the item id from button_texts_dict using the item name
+        item_id = format(int(button_texts_dict[item_name], 16), '04x')
         # Append to the code string
         code_str += weight_hex + item_id
 
@@ -181,25 +242,32 @@ def generate_gecko_code():
     
     print("\n" + gecko_code_header + code_str + "00000000" + gecko_code_footer)
 
-def on_generate_code():
+def on_generate_code(tab_parent):
     no_weight = 0
     checked_weights = 0
     total_weights = 0
+    #clear twice to avoid powershell leaving stuff in the window
     os.system('cls')  # Clear console for Windows
-    time.sleep(0.05)  # Pause for 0.2 seconds
+    os.system('cls')  # Clear console for Windows
 
-    selected_tab = event.widget.select()  # Get currently selected tab ID
-    selected_tab_text = event.widget.tab(selected_tab, "text")  # Get the text of the selected tab
+    selected_tab = tab_parent.select()  # Get currently selected tab ID
+    selected_tab_text = tab_parent.tab(selected_tab, "text")  # Get the text of the selected tab
 
-    #choose button text keys from mp4 or mp5
+    # choose button text keys from mp4 or mp5
     if selected_tab_text == "MP4":
         button_text_keys = list(button_texts_mp4.keys())
+        button_vars_dict = button_vars["MP4"]
+        entry_boxes_dict = entry_boxes["MP4"]
+        check_buttons_dict = check_buttons["MP4"]
     elif selected_tab_text == "MP5":
         button_text_keys = list(button_texts_mp5.keys())
+        button_vars_dict = button_vars["MP5"]
+        entry_boxes_dict = entry_boxes["MP5"]
+        check_buttons_dict = check_buttons["MP5"]
 
-    for i, var in enumerate(button_vars):
-        current_weight = int(entry_boxes[i].get())
-        if check_buttons[i].cget('fg') == 'grey':
+    for i, var in enumerate(button_vars_dict):
+        current_weight = int(entry_boxes_dict[i].get())
+        if check_buttons_dict[i].cget('fg') == 'grey':
             continue
         total_weights += current_weight
         if var.get() == 1:
@@ -214,10 +282,10 @@ def on_generate_code():
     if no_weight == 1:
         print("All selected items must have weights!")
         return
-    for i, var in enumerate(button_vars):
-        if check_buttons[i].cget('fg') == 'grey':
+    for i, var in enumerate(button_vars_dict):
+        if check_buttons_dict[i].cget('fg') == 'grey':
             continue
-        current_weight = int(entry_boxes[i].get())
+        current_weight = int(entry_boxes_dict[i].get())
         if current_weight == 0:
             continue
         item_name = button_text_keys[i]  # Get the name of the checked item
@@ -225,11 +293,11 @@ def on_generate_code():
         if item_percentage != 0:
             print(f'{item_name}: {item_percentage:.2f}%')
 
-    checked_buttons = [i+1 for i, var in enumerate(button_vars) if var.get() == 1]
+    checked_buttons = [i + 1 for i, var in enumerate(button_vars_dict) if var.get() == 1]
 
     if not checked_buttons:
         return
-    generate_gecko_code()
+    generate_gecko_code(tab_parent)
 
 
 def on_checkbutton_change(index, tab_name):
@@ -260,10 +328,13 @@ def limit_size(P):
     else:
         return False
 
-def clear_options():
-    clear_selections()
-    for i, var in enumerate(button_vars):
-        check_buttons[i].config(fg='grey')
+def clear_options(tab_parent):
+    selected_tab = tab_parent.select()  # Get currently selected tab ID
+    selected_tab_text = tab_parent.tab(selected_tab, "text")  # Get the text of the selected tab
+    if selected_tab_text == "MP4":
+        clear_selections("MP4")
+    elif selected_tab_text == "MP5":
+        clear_selections("MP5")
 
 
 # Update the function that creates the grid for a given tab
@@ -349,21 +420,26 @@ def create_gui():
     global entry_boxes
 
     root = tk.Tk()
-    root.title("Mario Party 5 Capsule Pool Gen")
+    root.title("Mario Party 4/5 Item Pool Generator")
     root.iconbitmap("Miracle_Capsule.ico")
 
+    #set default size of window to be large enough for mp4/mp5 item grids
     root.geometry("600x520")  # Width x Height in pixels
+    tab_parent = ttk.Notebook(root)
 
-    save_button = tk.Button(root, text="Save CSV", command=save_csv)
+    #Create Save CSV button
+    save_button = tk.Button(root, text="Save CSV", command=lambda: save_csv(tab_parent))
     save_button.pack()
 
-    load_button = tk.Button(root, text="Load CSV", command=load_csv)
+    #Create Load CSV button
+    load_button = tk.Button(root, text="Load CSV", command=lambda: load_csv(tab_parent))
     load_button.pack()
 
-    clear_button = tk.Button(root, text="Clear All", command=clear_options)
+    #Create Clear All button
+    clear_button = tk.Button(root, text="Clear All", command=lambda: clear_options(tab_parent))
     clear_button.pack()
 
-    tab_parent = ttk.Notebook(root)
+
     mp4_tab = ttk.Frame(tab_parent)
     mp5_tab = ttk.Frame(tab_parent)
     tab_parent.add(mp4_tab, text="MP4")
@@ -377,7 +453,7 @@ def create_gui():
     create_mp5_grid(mp5_tab, root, "MP5")
     tab_parent.bind("<<NotebookTabChanged>>", lambda event: on_tab_changed(event, root))
 
-    generate_button = tk.Button(root, text="Generate Gecko Code", command=on_generate_code)
+    generate_button = tk.Button(root, text="Generate Gecko Code", command=lambda: on_generate_code(tab_parent))
     generate_button.pack()
 
     help_button = tk.Button(root, text="Help", command=open_help_window)
---
+++ mp4AsmSource/mp4RandomItemsFromItemSpacev3[1of3].asm
@@ -0,0 +1,118 @@
+#To be inserted at 80083878
+# Array of integer percentages (assuming 12 items)
+# Replace the values in this array with the desired percentages for each item
+bl afterPercentages
+nop #for python script alignment
+percentages:
+#Mini Mushroom
+.2byte 0x0100 #weight
+.2byte 0x0000 #item ID
+
+#Mushroom
+.2byte 0x0101 #weight
+.2byte 0x0001 #item ID
+
+#Super Mini Mushroom
+.2byte 0x0102 #weight
+.2byte 0x0002 #item ID
+
+#Super Mega Mushroom
+.2byte 0x0103 #weight
+.2byte 0x0003 #item ID
+
+#Mini Mega Hammer
+.2byte 0x0104 #weight
+.2byte 0x0004 #item ID
+
+#Warp Pipe
+.2byte 0x0105 #weight
+.2byte 0x0005 #item ID
+
+#Swap Card
+.2byte 0x0106 #weight
+.2byte 0x0006 #item ID
+
+#Sparky Sticker
+.2byte 0x0107 #weight
+.2byte 0x0007 #item ID
+
+#Gaddlight
+.2byte 0x0108 #weight
+.2byte 0x0008 #item ID
+
+#Chomp Call
+.2byte 0x0109 #weight
+.2byte 0x0009 #item ID
+
+#Bowser Suit
+.2byte 0x010A #weight
+.2byte 0x000A #item ID
+
+#Boo Crystal Ball
+.2byte 0x010B #weight
+.2byte 0x000B #item ID
+
+#Magic Lamp
+.2byte 0x010C #weight
+.2byte 0x000C #item ID
+
+
+afterPercentages:
+#make r7 hold pointer to percentages
+mflr r7
+addi r7, r7, 4 #skip over nop
+# Calculate the range of random values based on percentages
+li r3, 0
+li r4, 0
+loop_calc:
+    cmpwi r3, 52 # Exit at 13 * 4 (4 being size of .4byte)
+    beq- exit_calc
+    lhzx r5, r7, r3 # Load the percentage for the current item using r3 as index
+    add r4, r4, r5 # Add the percentage to the range of random values
+    addi r3, r3, 4 # Increment index by 4 (size of an integer)
+    b loop_calc
+exit_calc:
+
+# Call the random integer function
+lis r5, 0x8005
+ori r5, r5, 0xFB40 # 0x8005FB40 get rand int function
+mtctr r5
+mr r3, r4
+bctrl
+
+# rand integer in r3 in range of percentages array
+
+# Select the item based on the random integer
+li r4, 0
+li r5, 0
+loop_select:
+    cmpwi r5, 52 # Exit at 13 * 4 (4 being size of .4byte)
+    beq- exit_select
+    lhzx r6, r7, r5 # Load the percentage for the current item using r5 as index
+    sub r3, r3, r6 # Subtract the percentage from the random integer
+    cmpwi r3, 0 # Compare the updated random integer with 0
+    bge+ loop_increment # If the updated random integer is greater than or equal to 0, continue to the next item
+    b exit_select
+loop_increment:
+    addi r4, r4, 1 #item id increment
+    addi r5, r5, 4 # Increment index by 4 (size of an integer)
+    b loop_select
+exit_select:
+    mulli r4, r4, 4 #i * sizeof int
+    addi r4, r4, 2
+    lhzx r4, r7, r4 #get new item id
+exit:
+lis r5, 0x817F
+ori r5, r5, 0xFFF0
+stw r4, 0 (r5)
+cmpwi r4, 8
+blt- skipModelIncrement
+#there's a gap in the item models starting at gaddlight's item id (8)
+addi r4, r4, 1
+skipModelIncrement:
+lis r5, 0x0007
+ori r5, r5, 0x006d #base item id
+add r3, r4, r5
+li r4, 0 #restore from hook
+li r5, 0 #restore from hook
+li r5, 0 #restore from hook
