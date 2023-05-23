ce47565dc2096fb3dfbcdea12f8dc583f07fa638
Rainchus <rainmachus@gmail.com>
2023-05-22 19:51:16 -0500

    fix hardcoded path

--- build.bat
+++ build.bat
@@ -1,6 +1,20 @@
 @echo off
-rmdir bin /s /q
-pyinstaller --onefile --icon=ico/icon.ico --noupx --noconsole main.py --add-binary="ico/icon.ico;." --add-data "C:\Users\Nora\scoop\apps\python\current\Lib\site-packages\customtkinter;customtkinter/"
-REN dist bin
-cd bin && REN main.exe MP5CapsulePoolGen.exe && cd ..
+
+set "customtkinter_path=%USERPROFILE%\AppData\Local\Programs\Python\Python311\Lib\site-packages\customtkinter"
+set "bin_dir=bin"
+
+IF EXIST "%bin_dir%" (
+    rmdir "%bin_dir%" /s /q
+)
+
+pyinstaller --onefile --icon=ico/icon.ico --noupx --noconsole main.py --add-binary="ico/icon.ico;." --add-data "%customtkinter_path%;customtkinter/"
+
+IF EXIST dist (
+    REN dist "%bin_dir%"
+)
+
+IF EXIST "%bin_dir%\main.exe" (
+    cd "%bin_dir%" && REN main.exe MP5CapsulePoolGen.exe && cd ..
+)
+
 PAUSE
